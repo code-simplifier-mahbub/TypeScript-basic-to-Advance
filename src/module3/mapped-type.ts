fcd6fef79e5c1284f7d2de0cc06f2389eb93ada7
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-05-09 05:48:41 +0600

    class object property method define added

--- src/module3/mapped-type.ts
+++ src/module3/mapped-type.ts
@@ -11,31 +11,32 @@
 // const nameGet = obj['name']
 // console.log(nameGet)
 
-// simple work
-type Area = {
-    height: number;
-    width: number;
-}
-
-type A = Area['height'] //looked up type
-type B = keyof Area // union type. 'height' | 'width'
-
-//similar but organised work  with generic
-type Volume = {
-    height: number;
-    width: number;
-    depth: number;
-}
-
-// type Area1<Volume> = {
-//     [key in keyof Volume]: Volume[key]
+// ******// simple work
+// type Area = {
+//     height: number;
+//     width: number;
 // }
-// similer Area1 but generic work
-type Area1<T> = {
-   readonly  [key in keyof T]: T[key]
-}
 
-const area1: Area1<{height: number, width: string}> = {height: 23, width: 'ten'}
+// type A = Area['height'] //looked up type
+// type B = keyof Area // union type. 'height' | 'width'
+
+
+// ********//similar but organised work  with generic
+// type Volume = {
+//     height: number;
+//     width: number;
+//     depth: number;
+// }
+
+// // type Area1<Volume> = {
+// //     [key in keyof Volume]: Volume[key]
+// // }
+// // similer Area1 but generic work
+// type Area1<T> = {
+//    readonly  [key in keyof T]: T[key]
+// }
+
+// const area1: Area1<{height: number, width: string}> = {height: 23, width: 'ten'}
 
 
 
