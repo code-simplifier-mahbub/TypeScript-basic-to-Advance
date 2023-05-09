69136e544716f16183c52ec26d46ebd1653b51f9
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-05-09 09:22:59 +0600

    inheritence in typescript

--- src/module4/class-object-pmeter.ts
+++ src/module4/class-object-pmeter.ts
@@ -1,24 +1,47 @@
-class Animal {
-    //property type 
-//  public name: string;      //this property is default pubic property
-//  public species: string;   //if we define public in constructor so we dont neeed declare here this property
-//  public sound: string;
+// class Animal {
+//     //property type 
+// //  public name: string;      //this property is default pubic property
+// //  public species: string;   //if we define public in constructor so we dont neeed declare here this property
+// //  public sound: string;
 
-    //constructor declaration
-    constructor(public name: string, public species: string, public sound: string){
-        // this.name = name;
-        // this.species= species; // compiler amake this.property er kaj kore dicce
-        // this.sound= sound
-    }
-    // normal function added because arrow function did not work
-    makeSound(){
-        console.log(`The ${this.name} say me ${this.sound}`);
-    }
-}
+//     //constructor declaration
+//     constructor(public name: string, public species: string, public sound: string){
+//         // this.name = name;
+//         // this.species= species; // compiler amake this.property er kaj kore dicce
+//         // this.sound= sound
+//     }
+//     // normal function added because arrow function did not work
+//     makeSound(){
+//         console.log(`The ${this.name} say me ${this.sound}`);
+//     }
+// }
 
-//called class with new constructor
-const dog = new Animal('UnderDog', 'Dog','Ghew Ghew');
-const cat = new Animal('zuto', 'cat', 'mew mew')
-//display result with called class function
-dog.makeSound();
-cat.makeSound();
+// //called class with new constructor
+// const dog = new Animal('UnderDog', 'Dog','Ghew Ghew');
+// const cat = new Animal('zuto', 'cat', 'mew mew')
+// //display result with called class function
+// dog.makeSound();
+// cat.makeSound();
+
+
+//*********simple code of animal */
+
+// class Animal {
+//     //constructor declaration
+//     constructor(
+//         public name: string,
+//         public species: string,
+//         public sound: string){       
+//     }
+//     // normal function added because arrow function did not work
+//     makeSound(){
+//         console.log(`The ${this.name} say me ${this.sound}`);
+//     }
+// }
+
+// //called class with new constructor
+// const dog = new Animal('UnderDog', 'Dog','Ghew Ghew');
+// const cat = new Animal('zuto', 'cat', 'mew mew')
+// //display result with called class function
+// dog.makeSound();
+// cat.makeSound();
